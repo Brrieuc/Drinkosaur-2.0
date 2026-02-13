c74cc5092590022b59705b7ac95f117eada01c68
Brrieuc <brieuc.pecqueraux@gmail.com>
2026-02-13 22:43:31 +0100

    revert: switch back to external award image URLs

--- src/constants/awards.ts
+++ src/constants/awards.ts
@@ -43,7 +43,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Rhumosaur',
         description: 'Player who drank the most rum',
         description_fr: 'Le joueur ayant bu le plus de rhum',
-        imageUrl: '/awards/Rhumosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinYKP3-bDTfV4WX3kjZ5MhqV55uk7AHPFUYKPOG67t5OMg1G7YHf1EflrBoaR7Eg9HoxptA0JCLTrzhMqeORBt5PAIi0tbDy0VkltGZv31c83pXRK3oqb7Nlnt_taPVsVfTFE_dd619LlUwL9VTEKItcHEBxE8CsbIpSpxF8Yg2z-wVgBMTGrq1GIdwHY/s320/Rhumosaur.png',
         category: 'most_rum',
     },
     {
@@ -52,7 +52,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Shotausor',
         description: 'Player who drank the most shots',
         description_fr: 'Le joueur ayant bu le plus de shots',
-        imageUrl: '/awards/Shotausor.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIiokHeNnu_I-y1xtBZd5bH1LLBMnUl-5owfA7O0QiE1XCiIvf5x2hFnVXQ1472u1nGwIyLgo_H_v1tfhYi9ec-wgx8QCo9i9oBId2k9kb4C0im6jvsvhZ4gUM795YQ8Xa1MqF-7HooSNPUWR4qvsNq5noeeMIUd9Wtzaw_ihb_Mc7kV2qVRrn8DeDJWE/s320/Shotausor.png',
         category: 'most_shots',
     },
     {
@@ -61,7 +61,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Vodkatausor',
         description: 'Player who drank the most vodka',
         description_fr: 'Le joueur ayant bu le plus de vodka',
-        imageUrl: '/awards/Vodkatosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh-E_2CXn5xCSWFq08PJtTf8dJAXxqmq_XNP4EkfMSbuxLsUSNQfaIWe74PcO6eQvu6amDSnrbFxBxaUDZi3Xo23ydvXfJdfw94Vc2fWmbZ8PfiKOyN8BUTnmTis1e0ScrRwoy6cSqV5wAfuqwIdPynUETa3CqYm4ukrR9Ez1RlnOSxwB_ufrgyq-tOPaw/s320/Vodkatosaur.png',
         category: 'most_vodka',
     },
     {
@@ -70,7 +70,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Partynausor',
         description: 'Player who drank on the most different days',
         description_fr: 'Le joueur ayant bu le plus de jours différents',
-        imageUrl: '/awards/Partynosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-LNegfETzzUgsiNDTxAPDDuhOKpdMWrIg7ijRl_I2tZQMmK4j6Dtg7NB-2G4bMBhhKL0W5h9jjaUO5BZu-Apsh4gBtT8PcSgboe3TgKwTkaGtVzuuqwzlVz03uvT8B0KKr5OkX0Pqxrifu_7gP2hvpn-jD8bDFOsJmO9k53SrzLKnGyOOcXOKebvX6b0/s320/Partynosaur.png',
         category: 'most_party_days',
     },
     {
@@ -79,7 +79,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Vomitosaur',
         description: 'Player who reached the highest peak BAC',
         description_fr: 'Le joueur ayant atteint le plus haut pic d\'alcool',
-        imageUrl: '/awards/Vomitosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_go6CcwGmmGlKqMxaqRWhmeWvVPPGRo-0e8hK0lo8YaXcoyx9xPxOccul8jgX82NUQegbaN5-d2EFY2Yz1l446UQgvgAQCH8IsJBd_t29_hQ61PzQ1aaa1ix5rCX2lt_Yl6Ey9divgOEdRBMAJz-47tC6LsEbrkJaE4IAjwV6cgIYdet1IKxyraw4F4g/s320/Vomitosaur.png',
         category: 'highest_peak_bac',
     },
     {
@@ -88,7 +88,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Drunkosaur',
         description: 'Player who stayed drunk the longest without sobering up',
         description_fr: 'Le joueur ayant fait la plus longue durée sans redevenir sobre',
-        imageUrl: '/awards/Drunkosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi5aaZg9KtAlcKx-jHdlvKzX7s7oJVtIJo2ii6FoHMeI5OtcPlk3720AmX_9komex0f46COkoYMNWQHhgbXpr_pkd7woesJ8fzf-9tZ0AgoH5wWXDTf5kJwEttJdk9-DL-CmTyYZ3dVN_PkhYl1JK8vLXYnkA06cBCK6bzGkO2mo50pRrpT7qNVBvIE_3Q/s320/Drunkosaur.png',
         category: 'longest_drunk_duration',
     },
     {
@@ -97,7 +97,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Winausor',
         description: 'Player who drank the most wine',
         description_fr: 'Le joueur ayant bu le plus de vin',
-        imageUrl: '/awards/Winosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhH5YeFclXsoSwT47KUHjMfHIEr7grQKuowXroI3_7gxBrnEoUCl6fiLyjF-jaGZ4WqNps0YruQme-1nejTmqlGYuUfGGtfttCoRNSdlF7dHV0HbrY1D-KmHMHawMOerPG6wh1aS48U4wfiIj5ZJKtd5zD1ALQooRF98BpXtIKcP-_4OOhk4I4tLRcArdU/s832/Winosaur.png',
         category: 'most_wine',
     },
     {
@@ -106,7 +106,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Chugginosaur',
         description: 'Player who chugged the most drinks',
         description_fr: 'Le joueur ayant fait le plus de cul-sec',
-        imageUrl: '/awards/Chuginosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg7Kj2tychdAS6UKPwE7m6EHgnyyozvqOQ3z33HjhAZ06dlHRrD3QBMgPV4DrrhvIIhVuaJMEshuqpQWK04IYB9DubhLRgQNe3Qg8pOzdYcFaWaVsyEEAlcT4IdUnmECHw4uzBapFtxVqgM8o-1wtQ1qQJ2x4SK29YB9m5TClFxsH4uNlhAPWxgKBuHWTw/s320/Chuginosaur.png',
         category: 'most_chugs',
     },
     {
@@ -115,7 +115,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Champagnosaur',
         description: 'Player who drank the most champagne',
         description_fr: 'Le joueur ayant bu le plus de champagne',
-        imageUrl: '/awards/Champagnosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhr2_PPEcUNn6gsDBq44na5pSH9QL3lGR0-WNR2NnNH32xyc36HmkhwFlaWc_FIvmyrTycp67Y9XFsnWT0H2UvwgeyFM6lsDxB4jk5j6bbgsWxeMQPC2czFjMZFiz_nDg7xq_zQ-8_nBCLlJUj6gS9lDR1heQRzRaKwn_WiCezoSNOkBgCpyLx5-5pCmis/s320/Champagnosaur.png',
         category: 'most_champagne',
     },
     {
@@ -124,7 +124,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Beerosaur',
         description: 'Player who drank the most beer',
         description_fr: 'Le joueur ayant bu le plus de bière',
-        imageUrl: '/awards/Beerosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDGcorsAmxWY2WYHkpZ9HCILvv1r3M-eQdlZ_8lnS56ATrww8nxPRq8L82nDaBrCmCk4t7fiJ2b-G_0mBjmNXB0KQNCmCQasPgydpcwsuQ6LYpOcTrx_nc5AYHZGzzw32Sx9LiBobLjAx3wu8eQjYYQqYTdoz-OZYtrzClrDptoih_INPRaijHp7qTy_A/s320/Beerosaur.png',
         category: 'most_beer',
     },
     {
@@ -133,7 +133,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Gintosaur',
         description: 'Player who drank the most gin',
         description_fr: 'Le joueur ayant bu le plus de gin',
-        imageUrl: '/awards/Gintosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgyfjSD3r30iTiEPOvOVBs6DPSacBrxIUqInaXCna6LH9ibt1CGHH088M13FcyFZxm9LNCAM_aBRNebBLb-LtxbGUowDcd5K_7R9Uk7VCr3OFwCYYWExrtZUSMkgdkjp9dLBTMWIpOx9iZPveJegxvit4FdUkXcFbbYEBG_LtZl1GbHldQa2mrTTdr3Pfw/s320/Gintosaur.png',
         category: 'most_gin',
     },
     {
@@ -142,7 +142,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Sobrosaur',
         description: 'Player who drank the least (or didn\'t drink at all)',
         description_fr: 'Le joueur ayant le moins bu / tout joueur n\'ayant pas bu',
-        imageUrl: '/awards/Sobrosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiCpJmdalDFUrYnK036KNGLEJcQ_0UjGrctNcdrC1e0YZ6gN3LUXBwff1WX5DzwfQl88tCj9fSu3z0DCPMKpVWsP_U9YSdCQGXK5Qc3xgmE47RUKwy2BWHipRfOUoXU9x9UeEb72GR0dpZdgQm4igBbySLeLjB1z6F5cy85ZnmxPvwD8PRqCy4eGS3jfCw/s320/Sobrosaur.png',
         category: 'least_drinks',
     },
     {
@@ -151,7 +151,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Tequilausor',
         description: 'Player who drank the most tequila',
         description_fr: 'Le joueur ayant bu le plus de tequila',
-        imageUrl: '/awards/Tequilausor.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj2Zo3tqvJlXfbLT9HdpUurjHQcqCi6ITA0QpMq4d_0ESUK1hmhf8HdJsP-k4Uv4vxLwbcP7k-L2ER08TkxkL-JdaiVTUdJa6MekqbjFFL9s1XVmH8-1dg1tkw8653pOPH5Ymd7Bet4hkis0oBr4W4T6fT6dncH1xqOv9pGKBjV34M_l-V9rRq42kJm6M4/s320/Tequilausor.png',
         category: 'most_tequila',
     },
     {
@@ -160,7 +160,7 @@ export const AWARD_DEFINITIONS: AwardDefinition[] = [
         name_fr: 'Whiskosaur',
         description: 'Player who drank the most whisky',
         description_fr: 'Le joueur ayant bu le plus de whisky',
-        imageUrl: '/awards/Whiskosaur.png',
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjX0ozeIfTUd2NmEqkRsBExgK3LBiA4SKxwzWLZbg1KS1q3pzSoMV6RDX8072mSd1anDPi92cIiBbcN0Hes0GCDQOCU2VtnWHYWromFPLrlsMfeSPw8I96tl1UbPUIvSlKh6-PEzJZQjfuXamBq2NI8t7Q36i-AOl_6-Kmh2OibizhRzzkfrJFDY7mamAI/s320/Whiskosaur.png',
         category: 'most_whisky',
     },
 ];
